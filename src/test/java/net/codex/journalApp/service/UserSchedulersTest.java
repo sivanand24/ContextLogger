e35cd6825fc4b521bfa8f0ac6868c862f9e70d1b
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-07-24 23:31:54 +0530

    working on sentiment analysis

--- src/test/java/net/codex/journalApp/service/UserSchedulersTest.java
+++ src/test/java/net/codex/journalApp/service/UserSchedulersTest.java
@@ -3,7 +3,9 @@ package net.codex.journalApp.service;
 import net.codex.journalApp.scheduler.UserScheduler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class UserSchedulersTest {
 
     @Autowired
