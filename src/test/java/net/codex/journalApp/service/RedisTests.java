e7487d8c443c7b881d2245833e9b6e47f03b9975
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-07-25 11:58:48 +0530

    working on sentiment analysis

---
+++ src/test/java/net/codex/journalApp/service/RedisTests.java
@@ -0,0 +1,19 @@
+package net.codex.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+public class RedisTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Test
+    void testSendMail(){
+        redisTemplate.opsForValue().set("email","sivanandmishra24@gmail.com");
+        Object email = redisTemplate.opsForValue().get("email");
+
+    }
+}
