6fa914cc0ffe9b01cdb722233aa33cfa681d7cc8
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-07-27 22:50:09 +0530

    swagger implementation

--- src/main/java/net/codex/journalApp/config/SwaggerConfig.java
+++ src/main/java/net/codex/journalApp/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Arrays;
 import java.util.List;
 
-
+@Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI myCustomConfig(){
