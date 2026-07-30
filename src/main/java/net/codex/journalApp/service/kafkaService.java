6334d0ef87f4bf243b577cfe7fad57dcd9a9b25b
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-07-30 22:42:37 +0530

    implementing kafkaService.java

---
+++ src/main/java/net/codex/journalApp/service/kafkaService.java
@@ -0,0 +1,31 @@
+package net.codex.journalApp.service;
+
+import net.codex.journalApp.entity.JournalEntry;
+import net.codex.journalApp.entity.User;
+import net.codex.journalApp.repository.JournalEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+public class kafkaService {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
+    @Transactional
+    public void saveEntry(JournalEntry journalEntry, String userName) {
+        try{
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveUser(user);
+        } catch (Exception e){
+            System.out.println(e);
+            throw new RuntimeException("An error occurred while saving the entry.",e);
+        }
+    }
+}
