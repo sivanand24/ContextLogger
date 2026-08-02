a376f2ed393afc252618a12327328f9878bf69e0
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-08-02 11:43:54 +0530

    creating the method in kafka class

--- src/main/java/net/codex/journalApp/service/kafkaService.java
+++ src/main/java/net/codex/journalApp/service/kafkaService.java
@@ -30,17 +30,4 @@ public class kafkaService {
 
     }
 
-    @Transactional
-    public void saveEntryofUser(JournalEntry journalEntry, String userName) {
-        try {
-            User user = userService.findByUserName(userName);
-            journalEntry.setDate(LocalDateTime.now());
-            JournalEntry saved = journalEntryRepository.save(journalEntry);
-            user.getJournalEntries().add(saved);
-            userService.saveUser(user);
-        } catch (Exception e) {
-            System.out.println(e);
-            throw new RuntimeException("An error occurred while saving the entry.", e);
-        }
-    }
 }
