8a274389d554cae1759365ad70d3af76f40ba164
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-08-03 23:10:32 +0530

    creating the method in kafka class

---
+++ src/main/java/net/codex/journalApp/service/SentimentConsumerService.java
@@ -0,0 +1,20 @@
+package net.codex.journalApp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SentimentConsumerService {
+    @Autowired
+    private EmailService emailService;
+
+    @KafkaListener(topics = "weekly-sentiments", groupId = "weekly-sentiment-group")
+    public void consume(SentimentData sentimentData) {
+        sendEmail(sentimentData);
+    }
+
+    private void sendEmail(SentimentData sentimentData) {
+        emailService.sendEmail(sentimentData.getEmail(), "Sentiment for previous week", sentimentData.getSentiment());
+    }
+}
