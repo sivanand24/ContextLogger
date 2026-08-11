8f06bfe0f19191935f3545cf5a5187ebeb99f4b6
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-08-11 23:44:49 +0530

    adding username and password authentication in PublicController

--- src/main/java/net/codex/journalApp/controller/PublicController.java
+++ src/main/java/net/codex/journalApp/controller/PublicController.java
@@ -1,10 +1,19 @@
 package net.codex.journalApp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.codex.journalApp.entity.User;
 import net.codex.journalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
+@Slf4j
 @RestController
 @RequestMapping("/public")
 public class PublicController {
@@ -12,6 +21,9 @@ public class PublicController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @GetMapping("/health-check")
     public String healthCheck(){
@@ -25,6 +37,11 @@ public class PublicController {
 
     @PostMapping("/login")
     public void login(@RequestBody User user){
-    userService.saveNewUser(user);
+    try{
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+
+    } catch(Exception e){
+
     }
+}
 }
